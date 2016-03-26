0557f25fb7d5a3878cb7e54d6bbe10a13fa5805c
liuxini <liuxinihit@163.com>
2016-03-26 22:50:02 +0800

    基本完成要求

--- stage2/task2_9/task2_9.js
+++ stage2/task2_9/task2_9.js
@@ -19,9 +19,11 @@ window.onload=function(){
 	var input = document.querySelector("input");
 	var btn = document.querySelector('button');
 	var list = document.querySelector("ul");
-	addEvent("change",input,setTag);
+	addEvent("keyup",input,setTag);
 	addEvent("click",btn,setHobbies);
-	addEvent("onmouseover",ul,isDelete);
+	addEvent("mouseover",list,isDelete);
+	addEvent("mouseout",list,leave);
+	addEvent("click",list,Delete);
 };
 function render(index){
 	if(index === 0){
@@ -37,31 +39,45 @@ function render(index){
 	}
 }
 
-function setTag(){
-	var input= document.querySelector("input");
-	var reg = /[^0-9a-zA-Z\u4e00-\u9fa5]+/;
-	var insertTag = input.value.split(reg);
-	for( var i=0;i<insertTag.length;i++){
-		alert(insertTag[i]);
-		if( insertTag[i] in tags){
-			continue;
-		}else{
-			tags.push(insertTag[i]);
+function setTag(e){
+	if(e.keyCode === 13 || e.keyCode === 32 || e.keyCode === 188){
+		var input= document.querySelector("input");
+		var reg = /[^0-9a-zA-Z\u4e00-\u9fa5]+/;
+		var insertTag = input.value.split(reg);
+		var temp={};
+
+		for( var i=0;i<tags.length;i++ ){
+			temp[tags[i]] =1;
 		}
+		for( var i=0;i<insertTag.length;i++){
+			if( insertTag[i] ==="")continue;
+			if(  temp[insertTag[i]] ===1){
+				alert("重复了");
+				continue;
+			}else{
+				tags.push(insertTag[i]);
+			}
+		}
+		while(tags.length>10 ){
+			tags.shift();
+		}
+		render(1);
 	}
-	while(tags.length>10 ){
-		tags.shift();
-	}
-	render(1);
 }
 
 function setHobbies(){
 	var textarea = document.querySelector("textarea");
 	var reg = /[^0-9a-zA-Z\u4e00-\u9fa5]+/;
 	var insertHobbies = textarea.value.split(reg);
+	var temp={};
+	for( var i=0;i<hobbies.length;i++ ){
+		temp[hobbies[i]] =1;
+	}
+
 	for( var i=0;i<insertHobbies.length;i++){
-		alert(insertHobbies[i]);
-		if( insertHobbies[i] in hobbies){
+		if( insertHobbies[i] ==="")continue;
+		if( temp[insertHobbies[i]] ===1){
+			alert("重复了");
 			continue;
 		}else{
 			hobbies.push(insertHobbies[i]);
@@ -77,5 +93,29 @@ function isDelete(evevt){
 	var eventnow = event || window.event;
   var lastEle =  eventnow.target || eventnow.srcElement;
   var  queue  = document.querySelector("ul");
+  if( lastEle.nodeName.toLowerCase() == "li"){
+  	lastEle.setAttribute("class","onmouse");
+  	lastEle.innerHTML = "是否要删除"+lastEle.innerHTML; 	
+  }
+}
 
+function leave(){
+	var eventnow = event || window.event;
+  var lastEle =  eventnow.target || eventnow.srcElement;
+  var  queue  = document.querySelector("ul");
+  if( lastEle.nodeName.toLowerCase() == "li"){
+  	lastEle.setAttribute("class","tags");
+  	// alert(lastEle.innerHTML.slice(5,lastEle.innerHTML.length));
+  	lastEle.innerHTML = lastEle.innerHTML.slice(5,lastEle.innerHTML.length);
+  }
+}
+
+function Delete(evevt){
+	var eventnow = event || window.event;
+  var lastEle =  eventnow.target || eventnow.srcElement;
+  var  queue  = document.querySelector("ul");
+  if( lastEle.nodeName.toLowerCase() == "li"){
+    queue.removeChild(lastEle);
+    tags.splice(tags.indexOf(lastEle.innerHTML),1); 
+  }
 }
