ca4faf47dfca42d6c26ff04ac926065cc4a1143b
liuxini <liuxinihit@163.com>
2016-03-29 00:15:12 +0800

    doing

--- stage2/task2_11/task2_11.js
+++ stage2/task2_11/task2_11.js
@@ -1,4 +1,5 @@
 var tree = document.querySelector(".root");
+var stack = [];
 
 function delay(fn, args, t) {
 	var queue = [], self, timer;
@@ -24,13 +25,14 @@ function delay(fn, args, t) {
 	};
 	return self.delay(fn, args, t);
 }
-function setClass(node){ 
-	node.setAttribute("style","background-color:blue");
+ function setClass(node){ 
+ 	node.setAttribute("style","background-color:blue");
 }
 
 function removeClass(node){
 	node.removeAttribute("style");
 }
+
 
 function render(node){
 	if(this.delayChian)
@@ -38,15 +40,85 @@ function render(node){
 	else
 		this.delayChian=delay(setClass,node,0).delay(removeClass,node,500);
 }
+function find(node){
+	if(this.delayChian)
+		this.delayChian.delay(function(){nodenode.setAttribute("style","background-color:red");},node,250);
+	else
+		this.delayChian=delay(function(){nodenode.setAttribute("style","background-color:red");},node,250);;
+}
 
 function preorder(node){
-
+	while( node|| stack.length>0){ 
+		while( node){
+			if(node.className=='order') return;
+			render(node);
+			stack.push(node);
+			node = node.firstElementChild;
+		}
+		node = stack.pop();
+		node = node.nextSibling;
+		while( node!==null&&node.nodeType!==1){
+				node = node.nextSibling;
+			}	
+ }
 }
 
 function postorder(node){
-
+	while( node|| stack.length>0){ 
+		while( node){
+			if(node.className=='order') return;			
+			stack.push(node);
+			node = node.firstElementChild;
+		}
+		node = stack.pop();
+		render(node);
+		node = node.nextSibling;
+		while( node!==null&&node.nodeType!==1){
+				node = node.nextSibling;
+			}	
+ }
 }
 
 function levelorder(node){
+	stack.push(node);
+	while(stack.length>0){
+		node = stack.shift();
+		render(node);
+		var children = node.childNodes;
 
+		for( var i=0;i<children.length;i++){
+			if( children[i].nodeType===1){
+				stack.push(children[i]);
+			}
+		}
+	}
+}
+
+function search(node){
+	var input = document.querySelector("input");
+	if( input.value===""){
+		alert("please input");
+		return;
+	}
+	stack.push(node);
+	while(stack.length>0){
+		node = stack.shift();
+		render(node);
+		if( node.firstChild.data===input.value){
+			find(node);
+			return;
+		}
+		var children = node.childNodes;
+
+		for( var i=0;i<children.length;i++){
+			if( children[i].nodeType===1){
+				stack.push(children[i]);
+				if( children[i].firstChild.data==input.value){
+					find(node);
+					return;
+				}
+			}
+		}
+	}
+	this.delayChian.delay(function(){alert('然而并没找到');},null,500);
 }
