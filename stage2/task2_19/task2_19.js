71ffce3f1b22090556da1e4be289b600f5148350
unknown <>
2016-04-01 11:53:05 +0800

    doing task2_20

--- stage2/task2_19/task2_19.js
+++ stage2/task2_19/task2_19.js
@@ -18,9 +18,10 @@ var list=[
 ["中山大学","华南理工大学","南方科技大学"],
 ["浙江大学","宁波大学","杭州电子科技大学"]
 ];
-(function(e){
+(function(){
   var inschool = document.getElementById('in');
   var notschool = document.getElementById('notin');
+
   addEvent("click",inschool,function(){
      var school = document.querySelector(".inschool");
      document.querySelector(".select").classList.remove("select");
@@ -31,5 +32,24 @@ var list=[
      document.querySelector(".select").classList.remove("select");
      notin.classList.add('select');
   });
+   var option = document.getElementById('address');
+     addEvent("change",option,change);
   
 })()
+change();
+function change(){
+  var city = document.getElementById('address');
+  var school = document.getElementById('univesity');
+  school.innerHTML="";
+  for( var i=0;i<city.length;i++){
+    if(city[i].selected){
+      for( var j=0;j<list[i].length;j++){
+        var option = document.createElement("option");    //创建option
+        option.innerHTML = list[i][j];        //设置option的值  （i-1 是因为chlidNodes是从1开始的）
+        option.value = list[i][j];
+        school.appendChild(option);
+      }
+    }
+  }
+}
+
---
+++ stage2/task2_20/task2_20.js
@@ -0,0 +1,14 @@
+function addEvent( type, element, fun){
+  if(element.addEventListener){
+    addEvent = function(type, element, fun){
+      element.addEventListener(type,fun,false);};
+    }
+  else if( element.attachEvent){
+    addEvent = function(type,element,fun){
+      element.attachEvent('on'+type,fun,false);};
+  }else{
+    addEvent = function(type,element,fun){
+      element['on'+type] =fun;};
+   }
+   return addEvent(type,element,fun);
+}
