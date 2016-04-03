80a8b908118cd78fc884b98c3119641254ff5829
liuxini <liuxinihit@163.com>
2016-04-04 00:13:02 +0800

    unpate 22,23

--- stage2/task2_23/task2_23.js
+++ stage2/task2_23/task2_23.js
@@ -22,18 +22,42 @@ function addEvent( type, element, fun){
                       +"<td></td><td></td><td></td><td></td><td></td><td></td>";
   };
   str +="<div id=bottom class=bottom></div>"
-  table.innerHTML=str;
+  table.innerHTML=str; 
 })();
 
 (function(){
   var mymove = document.getElementById("bottom");
   var input = document.getElementById('input');
   var btn = document.getElementById('button');
-  mymove.style.top = "210px";
+  var refress = document.getElementById('refress');
+  var line =document.getElementById('line');
+   mymove.style.top = "210px";
   mymove.style.left = "224px";
+  addEvent("click",refress,function(){
+     mymove.style.top = "210px";
+     mymove.style.left = "224px";
+     input.innerHTML="";
+     line.innerHTML="";
+  })
+
   addEvent("click",btn,order);
+  addEvent("keyup",input,rownum);
+  addEvent("scroll",input,function(){
+    var temp = input.scrollTop;
+    line.scrollTop =temp;  
+  })
+  function rownum(){
+    var list = input.value.split("\n");
+    var arr = " ";
+    for( var i=0;i<list.length;i++){
+      arr +="<div class='linenum'>" + (i + 1) + "</div>";
+    }
+    line.innerHTML = arr;
+    var temp = input.scrollTop;
+   line.scrollTop =temp;  
+  }
   var move = {
-   turn:0,
+    turn:0,
     tunlef:function(){
       --this.turn;
       mymove.style.transform = "rotate(" + this.turn * 90 + "deg)";
@@ -69,17 +93,17 @@ function addEvent( type, element, fun){
       }
     },
     goLeft: function() {
-      if (parseInt( mymove.style.left) > 0) {
+      if (parseInt( mymove.style.left) > 52) {
          mymove.style.left = (parseInt( mymove.style.left) - 43) + 'px';
       }
     },
     goRight: function() {
-      if (parseInt( mymove.style.left) < 360) {
+      if (parseInt( mymove.style.left) < 412) {
          mymove.style.left = (parseInt( mymove.style.left) + 43) + 'px';
       }
     },
     goTop: function() {
-      if (parseInt( mymove.style.top) > 0) {
+      if (parseInt( mymove.style.top) > 50) {
          mymove.style.top = (parseInt( mymove.style.top) - 40) + 'px';
       }
     },
@@ -110,49 +134,87 @@ function addEvent( type, element, fun){
     }
   }
   
-function order(){
-  switch( input.value.trim().toLowerCase()){
-    case 'go':
-      move.go();
-      break;
-    case 'tun lef':
-      move.tunlef();
-      break;
-    case 'tun rig':
-      move.tunrig();
-    break;
-    case 'tun bac':
-      move.tunbac();
-    break;
-    case 'tra lef':
-      move.goLeft();
-    break;
-    case 'tra top':
-      move.goTop();
-    break;
-    case 'tra rig':
-      move.goRight();
-    break;
-    case 'tra bot':
-      move.goBack();
-    break;
-    case 'mov lef':
-      move.movLeft();
-    break;
-    case 'mov top':
-      move.movTop();
-    break;
-    case 'mov rig':
-      move.movRight();
-    break;
-    case 'mov bot':
-      move.movBottom();
-    break;
-    default:
-      alert("error input");
+  function order(){
+    var orderlist = input.value.trim().split("\n");
+    for( var i=0;i<orderlist.length;i++){
+      var temp = orderlist[i].split(" ");
+      if( !isNaN(temp[temp.length-1])){
+        var num = temp[temp.length-1];
+        temp.pop();
+        orderlist[i] = temp.join(" ");
+        run(orderlist[i],num,i);
+      }else{
+         run(orderlist[i],1,i);
+      }
     }
   }
-})();
 
+  function run(str,num,j){
+    switch( str.toLowerCase()){
+    case 'go':
+      for(var i=0; i<num; i++){
+        move.go();
+      }
+      break;
+    case 'tun lef':
+      for(var i=0; i<num; i++){
+        move.tunlef();
+      }
+      break;
+    case 'tun rig':
+      for(var i=0; i<num; i++){
+        move.tunrig();
+      }
+    break;
+    case 'tun bac':
+      for(var i=0; i<num; i++){
+        move.tunbac();
+      }
+    break;
+    case 'tra lef':
+      for(var i=0; i<num; i++){
+        move.goLeft();
+      }
+      break;
+    case 'tra top':
+      for(var i=0; i<num; i++){
+        move.goTop();
+      }
+      break;
+    case 'tra rig':
+      for(var i=0; i<num; i++){
+        move.goRight();
+      }
+      break;
+    case 'tra bot':
+      for(var i=0; i<num; i++){
+        move.goBack();
+      }
+      break;
+    case 'mov lef':
+      for(var i=0; i<num; i++){
+        move.movLeft();
+      }
+      break;
+    case 'mov top':
+      for(var i=0; i<num; i++){
+        move.movTop();
+      }
+      break;
+    case 'mov rig':
+      for(var i=0; i<num; i++){
+        move.movRight();
+      }
+      break;
+    case 'mov bot':
+      for(var i=0; i<num; i++){
+        move.movBottom();
+      }
+      break;
+    default:
+      var err = line.getElementsByTagName('div');
+      err[j].style.backgroundColor = "red";
+    }
+  }
 
-
+})()
