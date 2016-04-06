722019316f211346530a5c5a885ef500de3d4073
liuxini <liuxinihit@163.com>
2016-04-07 00:08:10 +0800

    24 find way

---
+++ stage2/task2_24/task2_24.js
@@ -0,0 +1,316 @@
+function addEvent( type, element, fun){
+  if(element.addEventListener){
+    addEvent = function(type, element, fun){
+      element.addEventListener(type,fun,false);};
+    }
+  else if( element.attachEvent){
+    addEvent = function(type,element,fun){
+      element.attachEvent('on'+type,fun,false);};
+  }else{
+    addEvent = function(type,element,fun){
+      element['on'+type] =fun;};
+   }
+   return addEvent(type,element,fun);
+}
+
+(function () {
+  var str="";
+  var table = document.getElementById("tbody");
+
+  for( var i=1;i<=15;i++){
+    str += "<tr><td>"+i+"</td><td></td><td></td><td></td><td></td>"
+                    +"<td></td><td></td><td></td><td></td><td></td>"
+                    +"<td></td><td></td><td></td><td></td><td></td><td></td>";
+  };
+  table.innerHTML=str; 
+})();
+
+(function(){
+  var mymove = document.getElementById("bottom");
+  var input = document.getElementById('input');
+  var btn = document.getElementById('button');
+  var refress = document.getElementById('refress');
+  var line =document.getElementById('line');
+  var tbody = document.querySelector("tbody");
+  var position = {};
+   mymove.style.top = "242px";
+  mymove.style.left = "242px";
+  addEvent("click",refress,function(){
+     mymove.style.top = "242px";
+     mymove.style.left = "242px";
+     input.innerHTML="";
+     line.innerHTML="";
+  });
+
+  addEvent("click",btn,order);
+  addEvent("keyup",input,rownum);
+  addEvent("scroll",input,function(){
+    var temp = input.scrollTop;
+    line.scrollTop =temp;  
+  });
+  function rownum(){
+    var list = input.value.split("\n");
+    var arr = " ";
+    for( var i=0;i<list.length;i++){
+      arr +="<div class='linenum'>" + (i + 1) + "</div>";
+    }
+    line.innerHTML = arr;
+    var temp = input.scrollTop;
+   line.scrollTop =temp;  
+  }
+  var move = {
+    turn:0,
+    tunlef:function(){
+      --this.turn;
+      mymove.style.transform = "rotate(" + this.turn * 90 + "deg)";
+    },
+    tunrig:function(){
+      ++this.turn;
+      mymove.style.transform = "rotate(" + this.turn * 90 + "deg)";
+    },
+    tunbac:function(){
+      this.turn = this.turn + 2;
+      mymove.style.transform = "rotate(" + this.turn * 90 + "deg)";
+    },
+    build:function(){
+      if (this.turn % 4 == 3 || this.turn % 4 == -1){  //left
+        if (parseInt(mymove.style.left) > 82) {
+          var x = parseInt( (parseInt(mymove.style.left)-82)/40 )+1;
+          var y = parseInt( (parseInt(mymove.style.top)-42)/40 );
+          if( position["x"]===y){
+            console.log("墙已存在");
+          }else{
+            position.x = y;
+            tbody.childNodes[y].childNodes[x].setAttribute("class","block");
+          }
+        }else{
+          console.log("无足够空间");
+        }
+      }
+      if (this.turn % 4 == 2 || this.turn % 4 == -2) {  //bottom
+        if (parseInt(mymove.style.top) < 602) {
+          var x = parseInt( (parseInt(mymove.style.left)-42)/40 )+1;
+          var y = parseInt( (parseInt(mymove.style.top)-2)/40 );
+          if( position["x"]===y){
+            console.log("墙已存在");
+          }else{
+            position.x = y;
+            tbody.childNodes[y].childNodes[x].setAttribute("class","block");
+          }
+        }else{
+          console.log("无足够空间");
+        }
+      }
+      if  (this.turn% 4 == 1 || this.turn% 4 == -3) {  //right if (parseInt(mymove.style.left) < 612) {
+         if (parseInt(mymove.style.left) < 602) {
+          var x = parseInt( (parseInt(mymove.style.left)-2)/40 )+1;
+          var y = parseInt( (parseInt(mymove.style.top)-42)/40 );
+          if( position["x"]===y){
+            console.log("墙已存在");
+          }else{
+            position.x = y;
+            tbody.childNodes[y].childNodes[x].setAttribute("class","block");
+          }
+        }else{
+          console.log("无足够空间");
+        }
+      }
+      if (this.turn % 4 == 0){   //top
+        if (parseInt(mymove.style.top) > 82) {
+          var x = parseInt( (parseInt(mymove.style.left)-42)/40 )+1;
+          var y = parseInt( (parseInt(mymove.style.top)-82)/40 );
+          if( position["x"]===y){
+            console.log("墙已存在");
+          }else{
+            position.x = y;
+            tbody.childNodes[y].childNodes[x].setAttribute("class","block");
+          }
+        }else{
+          console.log("无足够空间");
+        }
+      }
+    },
+    go:function(){
+      if (this.turn % 4 == 3 || this.turn % 4 == -1) {
+        if (parseInt(mymove.style.left) > 52) {
+          mymove.style.left = (parseInt(mymove.style.left) - 40) + 'px';
+        }
+      }
+      if (this.turn % 4 == 2 || this.turn % 4 == -2) {
+        if (parseInt(mymove.style.top) < 602) {
+          mymove.style.top = (parseInt(mymove.style.top) + 40) + 'px';
+        }
+      }
+      if (this.turn% 4 == 1 || this.turn% 4 == -3) {
+        if (parseInt(mymove.style.left) < 601) {
+          mymove.style.left = (parseInt(mymove.style.left) + 40) + 'px';
+        }
+      }
+      if (this.turn % 4 == 0) {
+        if (parseInt(mymove.style.top) > 42) {
+          mymove.style.top = (parseInt(mymove.style.top) - 40) + 'px';
+        }
+      }
+    },
+    goLeft: function() {
+      if (parseInt( mymove.style.left) > 52) {
+         mymove.style.left = (parseInt( mymove.style.left) - 40) + 'px';
+      }
+    },
+    goRight: function() {
+      if (parseInt( mymove.style.left) < 601) {
+         mymove.style.left = (parseInt( mymove.style.left) + 40) + 'px';
+      }
+    },
+    goTop: function() {
+      if (parseInt( mymove.style.top) > 42) {
+         mymove.style.top = (parseInt( mymove.style.top) - 40) + 'px';
+      }
+    },
+    goBack: function() {
+      if (parseInt( mymove.style.top) < 602) {
+         mymove.style.top = (parseInt( mymove.style.top) + 40) + 'px';
+      }
+    },
+    movLeft: function() {
+       mymove.style.transform = "rotate(-90deg)";
+      this.goLeft();
+      this.turn = 3;
+    },
+    movRight: function() {
+       mymove.style.transform = "rotate(90deg)";
+      this.goRight();
+      this.turn = 1;
+    },
+    movTop: function() {
+       mymove.style.transform = "rotate(0deg)";
+      this.goTop();
+      this.turn = 0;
+    },
+    movBottom: function() {
+       mymove.style.transform = "rotate(180deg)";
+      this.goBack();
+      this.turn = 2;
+    }
+  };
+  
+  function order(){
+    var orderlist = input.value.split("\n");
+    var temp = orderlist[0].split(" ");
+    if( !isNaN(temp[temp.length-1])){
+        var num = temp[temp.length-1];
+        temp.pop();
+        orderlist[0] = temp.join(" ");
+        run(orderlist[0],num,0);
+      }else{
+         run(orderlist[0],1,0);
+      }
+    //   for( var i=1;i<orderlist.length;i++){
+    //     let j =i ;
+    //     setTimeout( function(){
+    //       var temp = orderlist[j].split(" ");
+    //       if( !isNaN(temp[temp.length-1])){
+    //         var num = temp[temp.length-1];
+    //         temp.pop();
+    //         orderlist[j] = temp.join(" ");
+
+    //         run(orderlist[j],num,j);
+    //       }else{
+    //            run(orderlist[j],1,j);
+    //       }  
+    //   },500*j);
+    // }  
+    var i=0;
+     var timer = setInterval(function(){
+       i++;
+       if( i<orderlist.length){
+        var temp = orderlist[i].split(" ");
+          if( !isNaN(temp[temp.length-1])){
+            var num = temp[temp.length-1];
+            temp.pop();
+            orderlist[i] = temp.join(" ");
+
+            run(orderlist[i],num,i);
+          }else{
+               run(orderlist[i],1,i);
+          } 
+         }else{
+           clearInterval(timer);
+         }
+     },500);
+  }
+
+  function run(str,num,j){
+    switch( str.toLowerCase()){
+     case 'build':
+      move.build();
+     break;
+     case 'bur':
+     break;
+    case 'go':
+      for(var i=0; i<num; i++){
+        move.go();
+      }
+      break;
+    case 'tun lef':
+      for(var i=0; i<num; i++){
+        move.tunlef();
+      }
+      break;
+    case 'tun rig':
+      for(var i=0; i<num; i++){
+        move.tunrig();
+      }
+    break;
+    case 'tun bac':
+      for(var i=0; i<num; i++){
+        move.tunbac();
+      }
+    break;
+    case 'tra lef':
+      for(var i=0; i<num; i++){
+        move.goLeft();
+      }
+      break;
+    case 'tra top':
+      for(var i=0; i<num; i++){
+        move.goTop();
+      }
+      break;
+    case 'tra rig':
+      for(var i=0; i<num; i++){
+        move.goRight();
+      }
+      break;
+    case 'tra bot':
+      for(var i=0; i<num; i++){
+        move.goBack();
+      }
+      break;
+    case 'mov lef':
+      for(var i=0; i<num; i++){
+        move.movLeft();
+      }
+      break;
+    case 'mov top':
+      for(var i=0; i<num; i++){
+        move.movTop();
+      }
+      break;
+    case 'mov rig':
+      for(var i=0; i<num; i++){
+        move.movRight();
+      }
+      break;
+    case 'mov bot':
+      for(var i=0; i<num; i++){
+        move.movBottom();
+      }
+      break;
+    default:
+      var err = line.getElementsByTagName('div');
+      err[j].style.backgroundColor = "red";
+    }
+  }
+
+})();
