378595311a08096d3aa58e91d9de8e2ecc171ea4
liuxini <liuxinihit@163.com>
2016-07-09 23:48:11 +0800

    add

---
+++ stage3/UI相册木桶/task3_7.js
@@ -0,0 +1,13 @@
+var Rowgallary = function  () {
+    this.element = document.querySelector('row-wrapper');
+    this.constant = this.element.clientWidth;
+    this.init();
+}
+
+rowgallary.prototype.init=function(){
+    element.html = "<div class='row-list'></div>"
+}
+
+rowgallary.prototype.addpic = function(photo){
+
+}
